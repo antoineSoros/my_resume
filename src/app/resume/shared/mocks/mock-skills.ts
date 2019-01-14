8e834b86816dc7f364c5c0100ea10d55d0c58667
backaligth <antoine.soros@orange.fr>
2019-01-14 14:03:52 +0100

    aesthetic stuff, mock-form mock-xp

---
+++ src/app/resume/shared/mocks/mock-skills.ts
@@ -0,0 +1,63 @@
+import {SkillModel} from '../models/skill.model';
+export const SKILLS: SkillModel[] = [
+  {
+    id: 1 ,
+    name: 'ANGLAIS',
+    level: 80,
+    logo: '/assets/logo/anglais.png'
+  },
+  {
+    id: 2,
+    name: 'HTML 5',
+    level: 80,
+    logo: '/assets/logo/html5.png'
+  },
+  {
+    id: 3,
+    name: 'CSS 3',
+    level: 80,
+    logo: '/assets/logo/css3.png'
+  },
+  {
+    id: 4,
+    name: 'C# 7',
+    level: 60,
+    logo: '/assets/logo/csharp.png'
+  },
+  {
+    id: 5,
+    name: 'JAVA 8',
+    level: 70,
+    logo: '/assets/logo/java.png'
+  },
+  {
+    id: 6,
+    name: 'PHP 7',
+    level: 65,
+    logo: '/assets/logo/php7.png'
+  },
+  {
+    id: 7,
+    name: 'ANDROID',
+    level: 70,
+    logo: '/assets/logo/android.jpg'
+  },
+  {
+    id: 8,
+    name: 'JAVASCRIPT 6',
+    level: 60,
+    logo: '/assets/logo/javascript.png'
+  },
+  {
+    id: 9,
+    name: 'ANGULAR 7',
+    level: 70,
+    logo: '/assets/logo/angular.png'
+  },
+  {
+    id: 10,
+    name: 'SYMFONY',
+    level: 60,
+    logo: '/assets/logo/symfony.png'
+  }
+];
