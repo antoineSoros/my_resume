b0bd94d4d9f971e27236795b22f9a5a7118a688a
backaligth <antoine.soros@orange.fr>
2019-01-21 13:59:36 +0100

    refactoring path

--- src/app/resume/shared/mocks/mock-links.ts
+++ src/app/resume/shared/mocks/mock-links.ts
@@ -4,13 +4,13 @@ export const LINKS: LinkModel[] = [
   {
     id: 1,
     name: 'Linkedin',
-    logo: '/assets/logo/linkedin.png',
+    logo: 'assets/logo/linkedin.png',
     url: 'https://linkedin.com/in/antoine-soros-7b1b5b176'
   },
   {
     id: 2,
     name: 'WeLoveDevs',
-    logo: '/assets/logo/welovdev.png',
+    logo: 'assets/logo/welovdev.png',
     url: 'https://welovedevs.com/app/fr/developer/d21vYJEZM0dRKnBeoSxEdaZOOly1'
   },
   {
